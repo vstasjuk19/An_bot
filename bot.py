259df8fce0293c09296eadf898adabccbdee081f
vstasjuk19 <vstasjuk19@gmail.com>
2025-04-16 17:20:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 
 # --- Змінні середовища ---
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не встановлено в середовищі!")
-if not ADMIN_ID:
-    raise ValueError("ADMIN_ID не встановлено або не є числом!")
-if not os.getenv("GOOGLE_CREDENTIALS"):
-    raise ValueError("GOOGLE_CREDENTIALS не встановлено!")
+ADMIN_ID = int(os.getenv("ADMIN_ID")
 
 # --- Підключення до Google Sheets ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
